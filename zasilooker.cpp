55ccf49a9efe5f8f80ce2f0bb71042ace0913cf4
cotara <nurruslan@yandex.ru>
2021-12-08 18:45:42 +0700

    глюк

---
+++ zasilooker.cpp
@@ -0,0 +1,101 @@
+#include "zasilooker.h"
+#include "ui_zasilooker.h"
+
+ZasiLooker::ZasiLooker(QWidget *parent,int diam, int num) : Looker(parent,num),  ui(new Ui::ZasiLooker), m_model(diam)
+{
+    ui->setupUi(this);
+    QPalette palette;
+    palette.setColor(QPalette::WindowText,Qt::black);
+    ui->lcdNumber->setPalette(palette);
+    ui->lcdNumber_2->setPalette(palette);
+    ui->lcdNumber_3->setPalette(palette);
+
+    QString fname = "Fault log "+QDateTime::currentDateTime().toString("yyyy-MM-dd_hh-mm-ss") + ".xlsx";
+}
+
+ZasiLooker::~ZasiLooker()
+{
+    delete ui;
+}
+
+
+void ZasiLooker::rePaint()
+{
+    QVector <double> m_data = data();
+    m_model = m_data.at(0);
+    m_vers = m_data.at(1);
+    m_ustMin = m_data.at(2);
+    m_ustMax = m_data.at(3);
+//    m_isHigh = m_data.at(10);
+//    m_highCur = m_data.at(11);
+//    m_ustCur = m_data.at(12);
+//    m_voltageCur = m_data.at(13);
+//    m_countBang = m_data.at(16);
+
+    ui->lcdNumber->display(m_ustCur/100);//Текущая уставка по напряжению
+    ui->lcdNumber_2->display(m_voltageCur/100);//Текущее амплитудное напряжение
+    ui->lcdNumber_3->display(m_countBang/100);//Количество пробоев
+
+    if(m_isHigh){
+        ui->isHighLabel->setStyleSheet(red);
+        ui->isHighLabel->setText("Высокое напряжение включено");
+    }
+    else {
+        ui->isHighLabel->setStyleSheet(lightgreen);
+        ui->isHighLabel->setText("Высокое напряжение отключено");
+    }
+
+}
+
+
+
+void ZasiLooker::on_setUstButton_clicked(){
+    QVector<unsigned short> data;
+    data.append(ui->setUstBox->value());
+    emit onSetReg(3, 1, data);
+}
+
+void ZasiLooker::on_onHighButton_clicked(bool checked){
+    QVector<unsigned short> data;
+    data.append(ui->onHighButton->isChecked());
+    emit onSetReg(10, 1, data);
+
+    if(checked)
+        ui->onHighButton->setText("Отключить");
+    else
+        ui->onHighButton->setText("Включить");
+}
+
+void ZasiLooker::writeToExcel()
+{
+    //записать новую запись в ексель
+    QXlsx::Document xlsx(fname);
+
+       qDebug()<<xlsx.read("B5");
+
+       QVariant temp = xlsx.read("B5");
+
+
+       m_current_xlsx_line =  temp.toInt();
+
+       QString fault_time = QDateTime::currentDateTime().toString("yyyy-MM-dd_hh-mm-ss");
+
+       QXlsx::Format font_bold;
+        font_bold.setFontBold(true);
+
+        xlsx.write(m_current_xlsx_line+8,1,"Окончание сессии "+fault_time,font_bold);
+
+
+
+        xlsx.save();
+}
+
+void ZasiLooker::on_DropDefectCountButton_clicked(){
+    QVector<unsigned short> data;
+    data.append(0);
+    emit onSetReg(16, 1, data);
+}
+
+
+
+
---
+++ zasilooker.h
@@ -0,0 +1,57 @@
+#ifndef ZASILOOKER_H
+#define ZASILOOKER_H
+
+#include <QWidget>
+#include "looker.h"
+#include <QDateTime>
+#include "\QtXlsxWriter-master\QtXlsxWriter-master\srcxlsxdocument.h"
+namespace Ui {
+class ZasiLooker;
+}
+
+class ZasiLooker : public Looker
+{
+    Q_OBJECT
+
+public:
+    explicit ZasiLooker(QWidget *parent = nullptr, int diam=5, int num=0);
+    ~ZasiLooker();
+    virtual void rePaint();
+
+private slots:
+
+    void on_setUstButton_clicked();
+    void on_DropDefectCountButton_clicked();
+    void on_onHighButton_clicked(bool checked);
+    void writeToExcel();
+private:
+    Ui::ZasiLooker *ui;
+    double m_model,m_vers,m_ustMin,m_ustMax,m_ustCur,m_highCur,m_voltageCur,m_countBang;
+    bool m_isHigh;
+    const QString lightgreen = "QLabel { background-color : lightgreen; }";
+    const QString yellow = "QLabel { background-color : yellow; }";
+    const QString red = "QLabel { background-color : red; color : white }";
+
+
+};
+
+#endif // ZASILOOKER_H
+
+//Адрес регистра	Хранимое значение	                         Режим доступа
+//0x0000              модель прибора                                  Чтение
+//0x0001              версия прошивки                                 Чтение
+//0x0002              минимальная уставка                             Чтение
+//0x0003              максимальная уставка                            Чтение
+//0x0004              сохраненное значение регулятора в точке 1       Чтение/ запись
+//0x0005              сохраненное значение регулятора в точке 2       Чтение/ запись
+//0x0006              сохраненное значение напряжения в точке 1       Чтение/ запись
+//0x0007              сохраненное значение напряжения в точке 2       Чтение/ запись
+//0x0008              режим работы прибора                            Чтение/ запись
+//0x0009              выход регулятора при калибровке                 Чтение/ запись
+//0x000A	        высокое вкл/выкл                	        Чтение/ запись
+//0x000B  	        Текущее значение реальное высокое               Чтение
+//0x000C	        Текущее значение уставки по напряжению	        Чтение/ запись
+//0x000D	        Текущее значение напряжения                     Чтение
+//0x000E	        Отображаемое значение                           Чтение
+//0x000F	        Текущее значение емкости                        Чтение
+//0x0010	        Текущее значение количества пробоев             Чтение/ запись
